bae684d3dc524a849c0629820462be6a202cb7b5
Aly Shukry <alyshukry.dev@gmail.com>
2025-10-03 00:54:45 +0300

    more testing

--- docs/_next/static/chunks/41600b6cf475aaeb.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/2e812a5b1b81f186.js",
-  "static/chunks/2cb0ad42209f5064.js",
-  "static/chunks/turbopack-7c2c2f1d4e74907d.js"
-])
--- docs/_next/static/chunks/cb1fcf110bb08b72.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/a22b30a854043680.js",
-  "static/chunks/2cb0ad42209f5064.js",
-  "static/chunks/turbopack-017fce9eaaa5c849.js"
-])
