456eaedf6cd3e37bed8d10ea955f5435a6cdc18a
Aly Shukry <alyshukry.dev@gmail.com>
2025-10-03 17:03:03 +0300

    test

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,5 +1,13 @@
+"use client"
+import { useEffect } from "react"
+
 export default function Home() {
-    return (
-        <p>hello</p>
-    )
+  useEffect(() => {
+    fetch("/api/docs.json")
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (<p>hello</p>);
 }
