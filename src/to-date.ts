0e903dda00c745b7ca96935b5b1c96e594ce6512
Sandip Gyawali <sandipgyawali10@gmail.com>
2025-11-09 20:00:25 +0545

    feat: added toDate method formatting date to an appropriate string

---
+++ src/to-date.ts
@@ -0,0 +1,23 @@
+interface Options {
+  date: Date;
+}
+
+/**
+ * Converts a given date object to an abbreviated string format
+ * @param options - Configuration Options
+ * @param options.date - Params containing date object
+ *
+ * @returns The string date format like DD/MM/YYYY
+ */
+export function toDate({ date }: Options): string {
+    const day = String(date.getDate()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0') // --month having 0th-indexed
+    const year = date.getFullYear()
+
+    console.log(day, month, year)
+
+    const formattedDate = `${day}/${month}/${year}`
+    return formattedDate
+}
+
+console.log(toDate({ date: new Date() }))
